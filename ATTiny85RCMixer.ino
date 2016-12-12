2502e8bdd7ece4ae06ad1131477bf1fd7db43598
BillyBag2 <bagshawwilliam@gmail.com>
2016-12-12 23:03:15 +0000

    Added some comments
    Added some comments.

--- ATTiny85RCMixer.ino
+++ ATTiny85RCMixer.ino
@@ -1,3 +1,35 @@
+// Inputs
+// ------
+// Rudder and throttle are servo inputs where a 1us pulse is 0% and a 2us pulse is 100%
+// * Note that for some transmitters you may have to increase the throw setting.
+//
+// Outputs
+// -------
+// M1 and M2 are high speed PWM outputs designed to go to a MOSFET then a motor. 
+// * A H-bridge would work but this mixer is for single direction motors.
+// M1 and M2 are not designated as left or right. In practice the transmitter's rudder direction can easily be swaped.
+//
+// Watchdog
+// --------
+// If a pulse of acceptable duration does not appear the Watchdog timer will fire. This is crude fail safe. For example motors will stop 1 second after reception is lost.
+//
+// Blocking, servo reading, main thread.
+// -------------------------------------
+// Input is read by the main thread using blocking functions. If a pulse is not an acceptable length it is re-read.
+// The watchdog will fire if this happens for more than 1 second.
+// Imputs are read alteratly. Not every pulse from the reciever is used.
+//
+// PWM output
+// ----------
+// PWM output is via the PWM hardware. It is set to the highest rate posible.
+//
+// Suggested pin out
+//  
+// 1 Reset    -|   |- 8 +V
+// 2 Throttle -|   |- 7 N/C
+// 3 Rudder   -|   |- 6 M2
+// 4 0V       -|   |- 5 M1
+//
 
 #include <avr/wdt.h>
 
@@ -11,11 +43,8 @@
 void setup() {
   pinMode(PORT_M1, OUTPUT);
   pinMode(PORT_M2, OUTPUT);
-  //pinMode(PORT_THROTTLE,INPUT);
-  //pinMode(PORT_RUD,INPUT);
-  wdt_enable(WDTO_1S);
-  //analogWrite(PORT_M2,0);
-  //analogWrite(PORT_M1,0);
+
+  wdt_enable(WDTO_1S); // WatchDog setup
 
   pwm_init();
 
@@ -40,13 +69,11 @@ void loop() {
   if(out > 255) out = 255;
   if(out < 0) out = 0;
   analogWrite(PORT_M1,out);
-  //TCCR0A = out;
   
   out = throttle - rud + 125;
   if(out > 255) out = 255;
   if(out < 0) out = 0;
   analogWrite(PORT_M2,out);
-  //TCCR0B = out;
   
   wdt_reset();
 }
